6c068294a3735504feb40c6a8a1258f180f2b160
Arkadiusz Ziobrowski <arekziobrowski@gmail.com>
2019-12-05 11:39:31 +0100

    Python 2.7 instead of 3.7

--- setup-python.sh
+++ setup-python.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 sudo yum -y install gcc openssl-devel bzip2-devel wget libffi-devel
 cd /tmp
-wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
-tar xzf Python-3.7.3.tgz
-cd Python-3.7.3
+wget https://www.python.org/ftp/python/2.7.17/Python-2.7.17.tgz
+tar xzf Python-2.7.17.tgz
+cd Python-2.7.17
 ./configure --enable-optimizations
 sudo make altinstall
-rm /tmp/Python-3.7.3.tgz
+rm /tmp/Python-2.7.17.tgz
+python2.7 -m ensurepip
