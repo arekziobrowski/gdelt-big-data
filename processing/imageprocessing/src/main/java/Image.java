e5f39a95271f47b7a4dc40a16a1058fe7e2e1ff6
Arkadiusz Ziobrowski <arekziobrowski@gmail.com>
2020-01-05 17:48:53 +0100

    #17 Image table mapping

---
+++ processing/imageprocessing/src/main/java/Image.java
@@ -0,0 +1,38 @@
+import java.io.Serializable;
+import java.util.Date;
+
+public class Image implements Serializable {
+
+    private String id;
+    private String url;
+    private Integer article_id;
+    private Date loadDate;
+
+    public Image(String id, String url, Integer article_id, Date loadDate) {
+        this.id = id;
+        this.url = url;
+        this.article_id = article_id;
+        this.loadDate = loadDate;
+    }
+
+    @Override
+    public String toString() {
+        return id + '|' + url + '|' + article_id + "|" + loadDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Integer getArticle_id() {
+        return article_id;
+    }
+
+    public Date getLoadDate() {
+        return loadDate;
+    }
+}
--- processing/imageprocessing/src/main/java/ImagePixelProcessor.java
+++ processing/imageprocessing/src/main/java/ImagePixelProcessor.java
@@ -4,39 +4,80 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.PairFlatMapFunction;
 import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.broadcast.Broadcast;
-import org.apache.spark.sql.DataFrame;
-import org.apache.spark.sql.SQLContext;
-import redis.clients.jedis.Jedis;
 import scala.Tuple2;
+
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.net.URI;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 
 
 public class ImagePixelProcessor {
 
+    private static final String ARTICLE_LOOKUP_PATH = "src/main/resources/article_lookup.dat";
+    private static final String ARTICLES_API_INFO_CLEANSED_PATH = "src/main/resources/articles-api-info-cleansed.csv";
+
     public static void main(String[] args) throws Exception {
-        
+
         SparkConf conf =
                 new SparkConf()
                 .setMaster("local[2]")
                 .setAppName("Image processing");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
+        JavaPairRDD<String, Integer> articleLookup =
+            sc.textFile(ARTICLE_LOOKUP_PATH)
+                .mapToPair(new PairFunction<String, String, Integer>() {
+                    @Override
+                    public Tuple2<String, Integer> call(String s) throws Exception {
+                        String[] spl = s.split("\\|");
+                        return new Tuple2<>(trimQuotes(spl[1]), Integer.parseInt(spl[0]));
+                    }
+                });
 
-        JavaRDD<String> paths = sc.parallelize(Arrays.asList("src/main/resources/test"));
-        JavaRDD<ImageMetadata> ims = paths.flatMap(new FlatMapFunction<String, ImageMetadata>() {
+        final Broadcast<Map<String, Integer>> b = sc.broadcast(articleLookup.collectAsMap());
+
+        JavaRDD<ArticleInfo> articleInfos =
+            sc.textFile(ARTICLES_API_INFO_CLEANSED_PATH)
+            .map(new Function<String, ArticleInfo>() {
+                @Override
+                public ArticleInfo call(String s) throws Exception {
+                    String[] spl = s.split("\\|");
+                    return new ArticleInfo(
+                            trimQuotes(spl[0]),
+                            trimQuotes(spl[1]),
+                            new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(trimQuotes(spl[2])),
+                            trimQuotes(spl[3]),
+                            trimQuotes(spl[4]),
+                            trimQuotes(spl[5]),
+                            trimQuotes(spl[6])
+                    );
+                }
+            }).cache();
+
+
+
+        JavaRDD<Image> images = articleInfos.map(new Function<ArticleInfo, Image>() {
             @Override
-            public Iterable<ImageMetadata> call(String s) throws Exception {
+            public Image call(ArticleInfo articleInfo) throws Exception {
+                return new Image(null, articleInfo.getImagePath(), b.value().get(articleInfo.getUrl()), new Date());
+            }
+        });
+
+        for(Image image : images.collect()) {
+            System.out.println(image);
+        }
+        
+        JavaRDD<ImageMetadata> ims = articleInfos.flatMap(new FlatMapFunction<ArticleInfo, ImageMetadata>() {
+            @Override
+            public Iterable<ImageMetadata> call(ArticleInfo articleInfo) throws Exception {
                 HashMap<Color, ImageMetadata> colorMap = new HashMap<>();
-                BufferedImage image = ImageIO.read(new File(s));
+                BufferedImage image = ImageIO.read(new File(articleInfo.getImagePath()));
 
                 if (image != null) {
                     for (int x = 0; x < image.getWidth(); x++) {
@@ -62,11 +103,15 @@ public class ImagePixelProcessor {
 
 
 
-        for (ImageMetadata i : ims.collect()) {
+        /*for (ImageMetadata i : ims.collect()) {
             //Color c = i.getC();
             System.out.println("ID: " + i.getColorId());
             //System.out.println("R:" + c.getR() + ", G: " + c.getG() + ", B: " + c.getB() + ", count: " + i.getCount());
-        }
+        }*/
 
+    }
+
+    private static String trimQuotes(String s) {
+        return s.substring(1, s.length() - 1);
     }
 }
